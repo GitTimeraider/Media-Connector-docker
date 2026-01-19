a052db470289c341e5c2b1587b52ca1e909a0778
Timeraider <57343973+GitTimeraider@users.noreply.github.com>
2026-01-19 14:03:29 +0100

    Fix

--- server/index.js
+++ server/index.js
@@ -89,7 +89,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
   
   // Wildcard route for React app (rate limited to prevent abuse)
-  app.get('*', healthLimiter, (req, res) => {
+  app.get('/*', healthLimiter, (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
   });
 }
