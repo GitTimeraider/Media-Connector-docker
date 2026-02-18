9df9b12df52ebbfd3e4899fa82fe7bb30ceb3690
Timeraider <57343973+GitTimeraider@users.noreply.github.com>
2026-02-18 19:08:06 +0100

    Dev addition

--- client/src/pages/Overview.js
+++ client/src/pages/Overview.js
@@ -351,9 +351,9 @@ function Overview() {
             <Typography variant="h5" gutterBottom sx={{ textTransform: 'capitalize' }}>
               {serviceType}
             </Typography>
-            <Grid container spacing={3} sx={{ mt: 1 }}>
+            <Grid container spacing={2} sx={{ mt: 1 }}>
               {instances.map((instance) => (
-                <Grid item xs={12} sm={6} md={4} key={instance.id}>
+                <Grid item xs={12} sm={6} md={4} lg={3} xl={2} key={instance.id}>
                   <ServiceCard serviceType={serviceType} instance={instance} />
                 </Grid>
               ))}
